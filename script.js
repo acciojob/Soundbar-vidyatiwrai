b02460b2687f934bc89f3fc97e6ddc5c40a75eac
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-16 15:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+    var audio = new Audio('sounds/' + soundFile);
+    audio.play();
+}
+
+function stopSound() {
+    var audioElements = document.getElementsByTagName('audio');
+    for (var i = 0; i < audioElements.length; i++) {
+        audioElements[i].pause();
+        audioElements[i].currentTime = 0;
+    }
+}
